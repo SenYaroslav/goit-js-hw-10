3df3c261f64de893d7c6f41690393cd951e37bd1
Iaroslav Seniuk <savuk7@icloud.com>
2022-11-04 16:50:38 +0200

    add some rows

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+let userInput = null;
+
+const inputFormRef = document.querySelector('input#search-box');
+
+inputFormRef.addEventListener(
+  'input',
+  debounce(handleInputForm, DEBOUNCE_DELAY)
+);
+
+function handleInputForm() {
+  userInput = inputFormRef.value.trim();
+  if (!userInput) {
+    return;
+  }
+  fetchCountries(userInput).then(data => {
+    console.log(data);
+  });
+}
+
+function addMarkup() {}
